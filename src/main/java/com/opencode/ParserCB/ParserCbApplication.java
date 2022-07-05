ea1eaabb759e072680e22ac92ba4b5fec2c487d3
Reezon7 <therezon777@gmail.com>
2022-07-05 11:12:01 +0400

    initialize project

---
+++ src/main/java/com/opencode/ParserCB/ParserCbApplication.java
@@ -0,0 +1,13 @@
+package com.opencode.ParserCB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserCbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParserCbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opencode/ParserCB/ParserCbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencode.ParserCB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserCbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
